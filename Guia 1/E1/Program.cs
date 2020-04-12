7e7024ff6e5a3ed6031183284ea9621716b1f15f
VenialgoSebastian <srveni@hotmail.com>
2020-04-12 18:12:46 -0300

    hola

--- Guia 1/E1/Program.cs
+++ Guia 1/E1/Program.cs
@@ -2,11 +2,52 @@
 
 namespace E1
 {
-    class Program
+    class num
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] vector = new int [10];
+            int aux = 0 ; 
+            int suma = 0 ;
+            double prome = 0 ; 
+            int menor = 0 ; 
+            
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Ingrese un numero: ");
+                vector[i] = Int32.Parse(Console.ReadLine());
+                suma += vector[i];
+                
+                if( vector[i]<menor || menor == 0)
+                {
+                    menor = vector[i];
+                }
+            }
+            
+            prome = suma/10;
+            Console.WriteLine("la suma es: " + suma +"\n el promedio es :"+ prome +"\n el numero menor es:"+menor);
+            for (int i = 0; i < 10; i++)
+            {
+
+                for (int j = 10-1; j >= i; j--)
+                {
+
+                    if (vector[j] > vector[i])
+                    {
+
+                        aux = vector[j];
+                        vector[j] = vector[i];
+                        vector[i] = aux;
+                        
+                    }
+
+                }
+
+            }
+            for(int i=0; i < 10; i++)
+            {
+                Console.WriteLine(vector[i]);
+            }
         }
     }
 }

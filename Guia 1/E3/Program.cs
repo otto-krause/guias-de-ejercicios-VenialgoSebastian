df592a619d7e7aaa7debf0dd0b1ec2263d1108a1
sebastian venialgo <srveni@hotmail.com>
2020-04-19 18:55:28 -0300

    Ejercicio 3

--- Guia 1/E3/Program.cs
+++ Guia 1/E3/Program.cs
@@ -2,11 +2,54 @@
 
 namespace E3
 {
-    class Program
+    class ingreso
     {
         static void Main(string[] args)
+        {   
+            int n1= 0;
+            Console.WriteLine("ingresa un numero ");
+            n1 = Int32.Parse(Console.ReadLine());
+            par(n1);
+            primo(n1);
+        }
+        
+        static public void par(int n1)
         {
-            Console.WriteLine("Hello World!");
+            
+            
+            if (n1 % 2 == 0)
+            {
+                Console.WriteLine("su numero es par");
+            }
+            else 
+            {
+               Console.WriteLine("su numero es inpar");
+            }
+        }
+
+        static public void primo(int n1)
+        {
+            int n=2;
+            bool band=true;
+            while( (band) &&(n <n1))
+            {
+                if((n1%n)==0)
+                {
+                    band=false;
+                }
+                else 
+                {
+                    n=n+1;
+                }
+            }
+            if(band)
+            {
+                Console.Write("es primo");
+            }
+            else
+            {
+                Console.Write("no es primo");
+            }
         }
     }
 }
